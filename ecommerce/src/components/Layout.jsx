efe0224442400ab7b25f903af9b8a6d879663737
Jorge Luis Garate Bascunan <extjgarate@transbank.cl>
2024-11-07 23:27:15 -0300

    error de ruta logo

--- ecommerce/src/components/Layout.jsx
+++ ecommerce/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import logo from "../assets/img/jorgeShop.svg"
+import logo from "/src/assets/img/jorgeShop.svg"
 
 function Layout({children}) {
   return (
